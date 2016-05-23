9d97bf754a92b8edbc42b44153351d4c5ceb12e6
Yossi <zozo123@users.noreply.github.com>
2016-05-23 22:41:45 +0300

    Update chartData.js

--- static/js/chartData.js
+++ static/js/chartData.js
@@ -1,21 +1,31 @@
-$(function(){
-  var chartData = {
-      labels: ["Jekyll", "Hugo", "Wintersmith"],
-      datasets: [
-          {
-              label: "Mean build time",
-              fillColor: "#E1EDD7",
-              strokeColor: "#E1EDD7",
-              highlightFill: "#C1D8AB",
-              highlightStroke: "#C1D8AB",
-              data: [784, 100, 5255]
-          }
-      ]
-  };
-  var ctx = $("#chartData").get(0).getContext("2d");
-  var myBarChart = new Chart(ctx).Bar(data1, {
-      scaleBeginAtZero : true,
-      responsive: true,
-      maintainAspectRatio: false,
-    }
-  );
+var ctx = document.getElementById("chart").getContext("2d");
+var data = {
+    labels: ["Jekyll", "Hugo", "Wintersmith", ],
+    datasets: [
+
+
+        {
+            label: "Mean build time",
+            fillColor: "#E1EDD7",
+            strokeColor: "#E1EDD7",
+            highlightFill: "#C1D8AB",
+            highlightStroke: "#C1D8AB",
+            data: [784, 100, 5255, ],
+        },
+
+    ]
+};
+
+var options = {
+    scaleBeginAtZero: true,
+    responsive: true,
+    maintainAspectRatio: false,
+    scaleShowGridLines: true,
+    scaleGridLineColor: "rgba(0,0,0,.05)",
+    scaleGridLineWidth: 1,
+    barShowStroke: true,
+    barStrokeWidth: 2,
+    barValueSpacing: 5,
+    barDatasetSpacing: 1,
+};
+var myLineChart = new Chart(ctx).Bar(data, options)
